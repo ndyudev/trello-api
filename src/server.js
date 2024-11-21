44620f8a994935e01077f544f418b9ed5ed38444
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-21 14:40:42 +0700

    connect_mongodb_env_variables3

--- src/server.js
+++ src/server.js
@@ -2,55 +2,41 @@
 
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
 const START_SERVER = async () => {
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 8017
-
-  // Khai báo biến db để sử dụng trong các route
-  const db = GET_DB()
-
   app.get('/', async (req, res) => {
-    try {
-      // Lấy danh sách collections trong database và log ra
-      const collections = await db.listCollections().toArray()
-      console.log(collections)
-      res.end('<h1>Hello World!</h1><hr>')
-    } catch (err) {
-      console.error('Error fetching collections:', err.message)
-      res.status(500).send('Internal Server Error')
-    }
+    res.end('<h1>Hello World!</h1><hr>')
   })
 
-  app.listen(port, hostname, () => {
-    console.log(
-      `3.Hi nDyuDev , Back-End Server MERN Stack is running successfully at Host: ${hostname} and PORT: ${port}
-      `
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3.Hi ${env.AUTHOR} , Back-End Server MERN Stack is running successfully at Host: ${env.APP_HOST} and PORT: ${env.APP_PORT}`
     )
   })
 
-  // Thực hiện các tác vụ clean up trước khi ta dừng server lại
+  // Hook để đóng database khi thoát
   exitHook(() => {
-    console.log('Closing database connection...')
+    console.log('4.Closing database connection...')
     CLOSE_DB()
+    console.log('5.Goodbye! Server stopped successfully.')
   })
 }
 
-// Chỉ khi kết nối tới DB thành công thì mới Start Server Back-End lên.
 // Immediately-invoked / Anonymous Async Functions (IIFE)
 (async () => {
   try {
     console.log('1. Connecting to MongoDB Cloud Atlas...')
     await CONNECT_DB()
     console.log('2. Connected to MongoDB Cloud Atlas!')
+
     // Khởi động Server Back-End sau khi đã Connect Database thành công
     await START_SERVER()
   } catch (error) {
     console.error('Failed to start the server:', error.message)
-    process.exit(0) // Thoát tiến trình nếu xảy ra lỗi
+    process.exit(1) // Thoát tiến trình nếu xảy ra lỗi
   }
 })()
 
