0ca2b6e40b1aff99112061fff5db67fd9010797f
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-22 18:06:28 +0700

    Controller Handing

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -22,13 +22,10 @@ const createNew = async (req, res, next) => {
   })
 
   try {
-    console.log('req.body:', req.body)
     // Set abortEarly : false de truong hop co nhieu validation de tra ve tat ca loi
     await correctCondition.validateAsync(req.body, { abortEarly: false })
-
-    // next()
-
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Validation : API create new board' })
+    // Validate dữ liệu xong xuôi hộp lệ thì trỏ request đi tiếp sang Controller
+    next()
   } catch (error) {
     res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
       errors: new Error(error).message
