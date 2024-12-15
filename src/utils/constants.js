a73d469c19f04dc3f702cb8ee76e14ae85e74f54
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-12-15 20:01:31 +0700

    Debug

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 // Những domain được phép truy cập vào server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
-  'https://trello-web-lemon-two.vercel.app/'
+  'https://trello-web-lemon-two.vercel.app'
   // Không cần local host nữa vì file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
   //...vv sau này có domain nữa.
 ]
